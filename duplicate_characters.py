f1a3c78c1529119130ed8ba76cd4443cfd96b476
mango-days <77330707+mango-days@users.noreply.github.com>
2021-10-30 19:24:23 +0530

    Update duplicate_characters.py

--- duplicate_characters.py
+++ duplicate_characters.py
@@ -1,17 +1,7 @@
 str="abcabcdefgabc"
-check="" # set of unique characters
-size=len(str)
-index=1
-check+=str[0]
-while index<size :
-    temp=str[index]
-    check_index=0
-    flag=0
-    while check_index<len(check):
-        if temp==check[check_index]:
-            print(temp)
-            flag=1
-            break
-        check_index+=1
-    if flag==0: check+=temp
-    index+=1
+dict={}
+for x in str:
+    if x in dict.keys(): dict[x]+=1
+    else: dict[x]=1
+for x, y in dict.items():
+    if y>1: print(x,y)
